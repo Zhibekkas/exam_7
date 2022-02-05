9bd1e3c12137c1d5dae8a89bfb9f4a5b273f7005
zhibek <zhibek.ksmva@gmail.com>
2022-02-05 16:34:17 +0600

    views methods and html files were written for choice

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -8,3 +8,13 @@ class PollForm(forms.ModelForm):
     class Meta:
         model = Poll
         fields = ['question']
+
+
+class ChoiceForm(forms.ModelForm):
+
+    class Meta:
+        model = Choice
+        fields = ['choice_text']
+
+
+
--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from webapp.views.poll import (IndexPollView, PollView, EditView, CreationView, DeletePollView)
+from webapp.views.choice import (ChoiceCreateView, ChoiceUpdateView, ChoiceDeleteView)
 
 urlpatterns = [
     path('', IndexPollView.as_view(), name="poll_index"),
     path('poll/<int:pk>/', PollView.as_view(), name="poll_view"),
     path('poll/<int:pk>/edit/', EditView.as_view(), name="poll_edit"),
     path('poll/add/', CreationView.as_view(), name="poll_add"),
-    path('poll/<int:pk>/delete/', DeletePollView.as_view(), name="poll_delete")
-
+    path('poll/<int:pk>/delete/', DeletePollView.as_view(), name="poll_delete"),
+    path('poll/<int:pk>/choice/add/', ChoiceCreateView.as_view(), name='choice_add'),
+    path('choice/<int:pk>/edit/', ChoiceUpdateView.as_view(), name='choice_edit'),
+    path('choice/<int:pk>/delete/', ChoiceDeleteView.as_view(), name='choice_delete')
 
 
 
